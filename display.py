361e72d9cdeaee7baf224b6cf7605f2c530c9202
Terry Tian <97772315+tytia@users.noreply.github.com>
2025-05-17 01:55:16 +1000

    Initial commit

---
+++ display.py
@@ -0,0 +1,36 @@
+from typing import List, Tuple
+
+# horizontal ─
+# vertial │
+# tl corner ┌
+# tr corner ┐
+# bl corner └
+# br corner ┘
+# path #
+# road ◇
+# car ◈
+# occupied ▣
+# free ▢
+
+def print_grid(grid: List[List[str]], pos: Tuple[int, int]) -> None:
+    n, m = len(grid), len(grid[0])
+    print(f"┌{"─"*n*3}─┐")
+    for i in range(n):
+        s = "│"
+        for j in range(m):
+            s += " "
+            s += grid[i][j] if (i, j) != pos else "◈"
+            s += " "
+        s += " │"
+        print(s)
+    print(f"└{"─"*n*3}─┘")
+
+
+if __name__ == "__main__":
+    print_grid([
+        [" ", "◇", "▣", "▣", "▢"],
+        [" ", "◇", "▣", "▢", "▣"],
+        ["◇", "◇", "◇", "◇", "◇"],
+        [" ", "◇", " ", " ", " "],
+        [" ", "◇", " ", " ", " "],
+    ], (2, 1))
